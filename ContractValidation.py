b61f28fb7e7765f3e9abafd8e1693aee87184185
Gert Handschoewerker <gert.handschoewerker@hotmail.com>
2022-03-11 20:17:18 +0100

    basic validation

--- ContractValidation.py
+++ ContractValidation.py
@@ -3,6 +3,7 @@ from openpyxl.utils import get_column_letter
 from openpyxl.formula import Tokenizer
 import pandas as pd
 import time
+import ValidationFunctions as vf
 
 
 '''
@@ -16,89 +17,10 @@ read_file_Att.to_excel (r'C:\\Users\\Gert\\Documents\\Development\\excellekes\\f
 
 
 
-
-path1 = 'C:\\Users\\Gert\\Documents\\Development\\excellekes\\final\\MAC.xlsx'
-path2 = 'C:\\Users\\Gert\\Documents\\Development\\excellekes\\final\\ATT.xlsx'
-pathdest = 'C:\\Users\\Gert\\Documents\\Development\\excellekes\\final\\RentalContract.xlsx'
-
-
-def addToLastColumn(path, addition):
-	wb = load_workbook(filename=path)
-	wbs0 = wb.worksheets[0]
-	#print(wbs0.max_column)
-	char = get_column_letter(wbs0.max_column+1)
-	#print(char)
-	i = 1
-	for row in wbs0:
-		wbs0[char + str(i)] = addition
-		i = i+1
-	wb.save(path) 
-	print('Added ' + addition + ' ' + str(wbs0.max_row) + ' times')
-
-
-def vlookupToLastCollumn(path, title, sheet, formula):
-	wb = load_workbook(filename=path)
-	wbs0 = wb.worksheets[0]
-	#print(wbs0.max_column)
-	char = get_column_letter(wbs0.max_column+1)
-	#print(char)
-	i = 1
-	for row in wbs0:
-		#wbs0[char + str(i)] = "=VLOOKUP(AB2,'Machines + Attachments'!B:S,10,FALSE)"
-		wbs0[char + str(i)] = "=VLOOKUP(AB" + str(i) + ",'" + sheet + formula
-		i = i+1
-	wbs0[char + '1'] = title
-	wb.save(path) 
-	
-#VLOOKUP(AB2,'Machines + Attachments'!B:S,10,FALSE)"
-
-#Purpose = "'!B:L;10,FALSE)"
-
-def sheetCopyPaste(ws, destination):
-	for row in ws:
-		for cell in row:
-			destination[cell.coordinate].value = cell.value
-
-
-
-def rowMemory(ws):  #produce the list of items in the particular row
-        for row in ws.iter_rows(min_row=2):
-            yield [cell.value for cell in row]
-
-
-def combiningWorkbooks(title, destination, header, second):
-
-	wbh = load_workbook(filename=header)
-	wsh_0 = wbh.worksheets[0]
-
-	wb2 = load_workbook(filename=second)
-	ws2_0 = wb2.worksheets[0]
-
-	'''
-	wb3 = load_workbook(filename=third, read_only=True)
-	ws3_0 = wb3.worksheets[0]
-	ws3_0.delete_cols(1)
-
-	wb4 = load_workbook(filename=fourth, read_only=True)
-	ws4_0 = wb4.worksheets[0]
-	ws4_0.delete_cols(1)
-	'''
-	wbdestination = load_workbook(filename=destination)
-	wsdestination_1 = wbdestination.create_sheet(title)
-
-	
-	sheetCopyPaste(wsh_0, wsdestination_1)
-
-	list_to_append = list(rowMemory(ws2_0))
-	#print(list_to_append)
-	for items in list_to_append:
-		#print(items)
-		wsdestination_1.append(items)
-			
-
-	wbdestination.save(destination)
-	wbh.close()
-	wb2.close()
+path1 = 'FileBucket\\MAC.xlsx'
+path2 = 'FileBucket\\ATT.xlsx'
+pathdest = 'FileBucket\\RentalContract.xlsx'
+pathtest = 'FileBucket\\test.xlsx'
 
 
 machAttsheetTitle = 'Machines + Attachments'
@@ -106,30 +28,120 @@ machAttsheetTitle = 'Machines + Attachments'
 	
 # 	validation starters
 Purpose = "'!B:L,10,FALSE)"
+State = "'!B:L,11)"
+ObjectType = "'!B:AH,33)"
+InFleetStartDate = ""
+InFleetEndDate = "'!B:L,9)"
 
-#print('Start Preparing Machine and Attachment data')
-#time.sleep(1)
-#addToLastColumn(path1, 'MACHINE')
-#addToLastColumn(path2, 'ATTACHMENT')
-#time.sleep(1)
-'''
-wbh = load_workbook(filename=path1)
-wsh_0 = wbh.worksheets[0]
-wbdestination = load_workbook(filename=pathdest)
-wsdestination_1 = wbdestination.create_sheet("title")
+valcol_a = 'AddressLine2'
+valcol_b = 'ZipCode'
+valcol_c = 'City'
+valcol_d = 'CountryISO2'
+#valcol_e = 'Latitude'
+#valcol_f = 'Longitude'
+#valcol_g = 'EstimatedEndDate'
+#valcol_h = 'DeliveryEarliest'
+#valcol_h = 'DeliveryLatest'
+valcol_h = 'Purpose'
 
-sheetCopyPaste(wsh_0, wsdestination_1)
-wbdestination.save(pathdest)
-'''
 
-#combiningWorkbooks(machAttsheetTitle, pathdest, path1, path2)
-'''
-wb = load_workbook(filename=pathdest)
+ValidationRegime = int(input('What kind of validation do you want? 1 = Full, 2 = Combining and validation, 3 = File Validation only\n'))
+
+if ValidationRegime == 1:
+	print('Start Preparing Machine and Attachment data', flush=True)
+	time.sleep(1)
+	vf.addToLastColumn(path1, 'MACHINE')
+	vf.addToLastColumn(path2, 'ATTACHMENT')
+	time.sleep(1)
+
+	vf.combiningWorkbooks(machAttsheetTitle, pathdest, path1, path2)
+
+
+	wb = load_workbook(filename=pathdest)
+	wbs0 = wb.worksheets[0]
+
+	wbs0.auto_filter.ref = wbs0.dimensions
+	wbs0.freeze_panes = 'A2' 
+
+	print('Comparing data', flush=True)
+	vf.vlookupToLastCollumn(wbs0, 'Purpose', machAttsheetTitle, Purpose)
+	vf.vlookupToLastCollumn(wbs0, 'State', machAttsheetTitle, State)
+	vf.vlookupToLastCollumn(wbs0, 'ObjectType', machAttsheetTitle, ObjectType)
+	vf.vlookupToLastCollumn(wbs0, 'InFleetEndDate', machAttsheetTitle, InFleetEndDate)
+
+	wb.save(pathdest)
+
+
+elif ValidationRegime == 2:
+	vf.combiningWorkbooks(machAttsheetTitle, pathdest, path1, path2)
+
+
+	wb = load_workbook(filename=pathdest)
+	wbs0 = wb.worksheets[0]
+
+	wbs0.auto_filter.ref = wbs0.dimensions
+	wbs0.freeze_panes = 'A2' 
+
+	print('Comparing data', flush=True)
+	vf.vlookupToLastCollumn(wbs0, 'Purpose', machAttsheetTitle, Purpose)
+	vf.vlookupToLastCollumn(wbs0, 'State', machAttsheetTitle, State)
+	vf.vlookupToLastCollumn(wbs0, 'ObjectType', machAttsheetTitle, ObjectType)
+	vf.vlookupToLastCollumn(wbs0, 'InFleetEndDate', machAttsheetTitle, InFleetEndDate)
+	wb.save(pathdest)
+
+
+	print('Validating', flush=True)
+
+	vf.searchForBlanks(wb, wbs0, valcol_a)
+	vf.searchForBlanks(wb, wbs0, valcol_b)
+	vf.searchForBlanks(wb, wbs0, valcol_c)
+	vf.searchForBlanks(wb, wbs0, valcol_d)
+	vf.searchForBlanks(wb, wbs0, valcol_e)
+
+	wb.save(pathdest)
+
+
+elif ValidationRegime == 3:
+	wb = load_workbook(filename=pathdest)
+	wbs0 = wb.worksheets[0]
+
+	wbs0.auto_filter.ref = wbs0.dimensions
+	wbs0.freeze_panes = 'A2' 	
+
+	print('Comparing data', flush=True)
+
+	vf.vlookupToLastCollumn(wbs0, 'Purpose', machAttsheetTitle, Purpose)
+	vf.vlookupToLastCollumn(wbs0, 'State', machAttsheetTitle, State)
+	vf.vlookupToLastCollumn(wbs0, 'ObjectType', machAttsheetTitle, ObjectType)
+	vf.vlookupToLastCollumn(wbs0, 'InFleetEndDate', machAttsheetTitle, InFleetEndDate)
+
+	print('Validating', flush=True)
+
+	vf.searchForBlanks(wb, wbs0, valcol_a)
+	vf.searchForBlanks(wb, wbs0, valcol_b)
+	vf.searchForBlanks(wb, wbs0, valcol_c)
+	vf.searchForBlanks(wb, wbs0, valcol_d)
+	vf.searchForBlanks(wb, wbs0, valcol_e)
+
+
+
+
+
+	wb.save(pathdest)
+
+
+
+
+
+	'''
+
+
+
+
+wb = load_workbook(filename=pathtest)
 wbs0 = wb.worksheets[0]
-wbs0['BJ2'] = "=VLOOKUP(AB2,'Machines + Attachments'!B:S,10,FALSE)"
-wb.save(pathdest)
-'''
-#wbs0['BJ2'] = Tokenizer("""=VERT.ZOEKEN(AB1;'Machines + Attachments'!B:L;10)""")
+vf.searchForBlanks(wb, wbs0, 'borst')
 
-vlookupToLastCollumn(pathdest, 'Purpose', machAttsheetTitle, Purpose)
+#print(wbs0['C4'].value)
 
+wb.save(filename=pathtest) '''
---
+++ ValidationFunctions.py
@@ -0,0 +1,116 @@
+from openpyxl import workbook, load_workbook
+from openpyxl.utils import get_column_letter
+
+
+
+def addToLastColumn(path, addition):
+	wb = load_workbook(filename=path)
+	wbs0 = wb.worksheets[0]
+	#print(wbs0.max_column)
+	char = get_column_letter(wbs0.max_column+1)
+	#print(char)
+	i = 1
+	for row in wbs0:
+		wbs0[char + str(i)] = addition
+		i = i+1
+	wb.save(path) 
+	print('Added ' + addition + ' ' + str(wbs0.max_row) + ' times', flush=True)
+
+
+def vlookupToLastCollumn(sheet, title, sheetname, formula):
+	
+	#print(sheet.max_column)
+	char = get_column_letter(sheet.max_column+1)
+	#print(char)
+	i = 1
+	for row in sheet:
+		#sheet[char + str(i)] = "=VLOOKUP(AB2,'Machines + Attachments'!B:S,10,FALSE)"
+		sheet[char + str(i)] = "=VLOOKUP(AB" + str(i) + ",'" + sheetname + formula
+		i = i+1
+	sheet[char + '1'] = title
+
+	
+
+def sheetCopyPaste(ws, destination):
+	for row in ws:
+		for cell in row:
+			destination[cell.coordinate].value = cell.value
+
+
+
+def rowMemory(ws):  #produce the list of items in the particular row
+        for row in ws.iter_rows(min_row=2):
+            yield [cell.value for cell in row]
+
+
+def combiningWorkbooks(title, destination, header, second):
+
+	wbh = load_workbook(filename=header)
+	wsh_0 = wbh.worksheets[0]
+
+	wb2 = load_workbook(filename=second)
+	ws2_0 = wb2.worksheets[0]
+
+	wbdestination = load_workbook(filename=destination)
+	wsdestination_1 = wbdestination.create_sheet(title)
+
+	
+	sheetCopyPaste(wsh_0, wsdestination_1)
+
+	list_to_append = list(rowMemory(ws2_0))
+	#print(list_to_append)
+	for items in list_to_append:
+		#print(items)
+		wsdestination_1.append(items)
+		wsdestination_1.auto_filter.ref = wsdestination_1.dimensions
+		wsdestination_1.freeze_panes = 'A2'
+			
+
+	wbdestination.save(destination)
+	wbh.close()
+	wb2.close()
+	print('Everything is combined', flush=True)
+
+
+def searchForBlanks(wb, ws, header):
+	ws2 = wb.create_sheet(header + " irregularities")
+	for row in ws.iter_rows(min_row=1, max_row=1):
+		ws2.append((cell.value for cell in row))
+	check = 0
+
+	#Getting the header coordinates to check
+	for col in ws.columns:
+		column = get_column_letter(col[0].column)
+		#print(column)
+		for cell in col:
+			if str(cell.value) == str(header):
+				char = column
+				#print(char)
+
+				#checking if the dedicated columm contains a irregularity and than copying the whole row
+				for row in ws.rows:
+					#print(str(row[0].row))
+					if ws[char + str(row[0].row)].value == 0 or ws[char + str(row[0].row)].value == None:
+
+						#needed to add a dumb calculation to remove the sheet again. Could not make it work with searching for A1 as empty cell. 
+						check = check + 1
+						#print(ws[char+ str(row[0].row)])
+						ws2.append((cell.value for cell in row))
+
+
+#if there is no need we can delete the sheet again.
+	if check == 0:
+		print('There where no irregularities in ' + header)
+		wb.remove_sheet(ws2)
+
+	elif check != 0:
+		print('There where irregularities in ' + header)
+		ws2.auto_filter.ref = ws2.dimensions
+		ws2.freeze_panes = 'A2' 
+		#i could optimise this to insert the headers here but yeah most of the times there will be issues here
+
+
+
+
+
+
